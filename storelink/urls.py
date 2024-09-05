f79907ce8d9fd578a29c3dd83d97549459b7969f
barkode <konstantinks@gmail.com>
2024-09-05 12:46:54 +0100

    Create connection to PostgreSQL database

--- storelink/urls.py
+++ storelink/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from main.views import main
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("main.urls"), name="main"),
+    # path("", include("main.urls"), name="home"),
+    path("", main, name="home"),
 ]
