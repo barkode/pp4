69c6b255957b3962b619b478a4925cc50f8aafe2
barkode <konstantinks@gmail.com>
2024-09-10 10:42:22 +0100

    Change header, title and index title on admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# Change titles on the administrator page
+
+admin.site.site_title = "Store your links"
+admin.site.site_header = "StoreLinks Administrator panel"
+admin.site.index_title = "Administration panel"
