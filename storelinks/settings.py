a16c74fa4b2d1da730bd000d629f089b65924ce7
barkode <konstantinks@gmail.com>
2024-09-11 12:20:47 +0100

    Add whitenoise package to deploy static files

--- storelinks/settings.py
+++ storelinks/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 # Turn off django debug toolbar when running test
