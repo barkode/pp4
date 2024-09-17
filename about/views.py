ec214c95a8a02a3339cc731773c8b44f495877ff
barkode <konstantinks@gmail.com>
2024-09-17 16:15:22 +0100

    Add view to about app

--- about/views.py
+++ about/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views import generic
+
+from .forms import CollaborateRequest
+from .models import About
 
 
-def about(request):
-    context = {"title": "About", "movie": "Page about US"}
+class AboutPage(generic.FormView, generic.TemplateView):
+    template_name = "about/about.html"
+    form_class = CollaborateRequest
+    success_url = reverse_lazy('about')
 
-    return render(request, "about/about.html", context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['about'] = About.objects.all().order_by('-updated_on').first()
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, 'Collaboration request received! I endeavor to respond within 2 working days.')
+        return super().form_valid(form)
+
