653f2c086a6212b60ccf22bd5163e1e03044a7c0
barkode <konstantinks@gmail.com>
2024-09-25 00:11:02 +0100

    Enhance profile form functionality
    Added CSRF token, server-side validation, and error display to the profile form. Included dynamic image display based on user data. Introduced `UserProfileForm` in forms.py to manage user profile updates.

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,24 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    """
+    This class represents a user login form used for authentication.
+
+    Attributes:
+        username: A character field for the username input.
+        password: A character field for the password input.
+
+    Meta:
+        model: The User model which is associated with this form.
+        fields: Specifies the fields included in the form, which are 'username' and 'password'.
+
+    Classes:
+        Meta: An inner class to specify the meta-data for the UserLoginForm.
+    """
     username = forms.CharField()
     password = forms.CharField()
 
@@ -14,6 +28,35 @@ class UserLoginForm(AuthenticationForm):
 
 
 class UserRegisterForm(UserCreationForm):
+    """
+    User registration form class extending UserCreationForm.
+
+    class UserRegisterForm(UserCreationForm):
+
+    Meta class configuration to define model and fields for the form.
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    Form field for first name.
+    first_name = forms.CharField()
+
+    Form field for last name.
+    last_name = forms.CharField()
+
+    Form field for username.
+    username = forms.CharField()
+
+    Form field for email.
+    email = forms.EmailField()
+
+    Form field for password.
+    password1 = forms.CharField()
+
+    Form field for password confirmation.
+    password2 = forms.CharField()
+    """
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
@@ -24,3 +67,17 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+
+
+class UserProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('image', 'first_name', 'last_name', 'email', 'username' )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+    username = forms.CharField()
+
+
