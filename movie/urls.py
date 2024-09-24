78ff1bd08bcc4b7b937aed5f6d9d37ffcda7aba1
barkode <konstantinks@gmail.com>
2024-09-25 00:54:14 +0100

    Fix URL name for genre-specific movie catalog
    Rename the URL name from "movie_catalog" to "movie_genre" for paths including a genre slug. This helps to differentiate between general and genre-specific catalog views.

--- movie/urls.py
+++ movie/urls.py
@@ -6,6 +6,6 @@ app_name = "movies"
 urlpatterns = [
     path("", views.movie_catalog, name="movie_catalog"),
     path("search/", views.movie_catalog, name="movie_search"),
-    path("<slug:genre_slug>/", views.movie_catalog, name="movie_catalog"),
+    path("<slug:genre_slug>/", views.movie_catalog, name="movie_genre"),
     path("movie/<slug:movie_slug>/", views.movie_detail, name="movie_detail"),
 ]
