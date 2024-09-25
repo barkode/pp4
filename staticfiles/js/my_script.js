7c307d8874bf446bc61d49072d4cbc272806c2f8
barkode <konstantinks@gmail.com>
2024-09-25 12:18:25 +0100

    Add new static assets and scripts
    This commit introduces various new static assets, including icons for Facebook, GitHub, and Google, a favicon, and different JavaScript scripts. These assets are important for enhancing the application's UI and functionality.

---
+++ staticfiles/js/my_script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const notification = document.querySelector('#notification');
+    if (notification) {
+        setTimeout(() => {
+            notification.style.transition = 'opacity 0.5s';
+            notification.style.opacity = '0';
+            setTimeout(() => {
+                notification.style.display = 'none';
+            }, 500); // Після завершення перехідного періоду
+        }, 7000);
+    }
+});
