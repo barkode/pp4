eb981014f9170cdebd01ea423bcaca09f5bbc722
barkode <konstantinks@gmail.com>
2024-12-20 02:23:50 +0000

    Add dynamic title to About and Contact pages
    Updated the `about` and `contact` views to include a `title` variable in the context, ensuring dynamic page titles for better user experience and SEO. No changes to layout or functionality beyond the addition of this variable.

--- about/views.py
+++ about/views.py
@@ -3,35 +3,44 @@ from .models import About
 from .forms import CollaborateForm
 from django.contrib import messages
 
+
 def about(request):
-    """
-    Renders the About page
-    """
-    about_field = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
-    return render(
-        request,
-        "about/about.html",
-        {
-            "about": about_field,
-            "collaborate_form": collaborate_form
-            },
-        )
+	"""
+	Renders the About page
+	"""
+	about_field = About.objects.all().order_by('-updated_on').first()
+	collaborate_form = CollaborateForm()
+	title = "StoreLinks - About"
+
+	return render(
+		request,
+		"about/about.html",
+		{
+			"about": about_field,
+			"collaborate_form": collaborate_form,
+			"title": title,
+			},
+		)
+
 
 def contact(request):
-    """
-    Renders the Contact page
-    """
-    if request.method == "POST":
-        collaborate_form = CollaborateForm(request.POST)
-        if collaborate_form.is_valid():
-            collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
-    collaborate_form = CollaborateForm()
-    return render(
-        request,
-        "about/contact.html",
-        {
-            "collaborate_form": collaborate_form
-            },
-        )
+	"""
+	Renders the Contact page
+	"""
+	if request.method == "POST":
+		collaborate_form = CollaborateForm(request.POST)
+		if collaborate_form.is_valid():
+			collaborate_form.save()
+			messages.add_message(request, messages.SUCCESS,
+			                     "Collaboration request received! I endeavour to respond within 2 working days.")
+	collaborate_form = CollaborateForm()
+	title = "StoreLinks - Contact"
+
+	return render(
+		request,
+		"about/contact.html",
+		{
+			"collaborate_form": collaborate_form,
+			"title": title,
+			},
+		)
